d590dda2f46b15a94132549e920f27e64193b20e
Omar A Mukadam <om@princeton.edu>
2016-07-29 15:50:06 -0400

    Added String prototype functions to calculate the width and height of text. Labels are now centered and positioned directly beneath the datapoints.

--- script.js
+++ script.js
@@ -9,19 +9,60 @@ var input = {
     }
 };
 
+//Acquire the width of a string using a particular font
+String.prototype.width = function(font) {
+    var f = font || '12px arial';
 
+    var o = $('<div>' + this + '</div>')
+        .css({
+            'position': 'absolute',
+            'float': 'left',
+            'white-space': 'nowrap',
+            'visibility': 'hidden',
+            'font': font
+        })
+        .appendTo($('body'));
 
+    var divWidth = o.width();
+
+    o.remove();
+
+    return divWidth;
+}
+
+//Acquire the height of a string using a particular font
+String.prototype.height = function(font) {
+    var f = font || '12px arial';
+
+    var o = $('<div>' + this + '</div>')
+        .css({
+            'position': 'absolute',
+            'float': 'left',
+            'white-space': 'nowrap',
+            'visibility': 'hidden',
+            'font': font
+        })
+        .appendTo($('body'));
+
+    var divHeight = o.height();
+
+    o.remove();
+
+    return divHeight;
+}
 
 var entities = JSON.parse(input.Item.entities.S);
 console.log(JSON.stringify(entities, null, "\t"));
 
 
-var length = entities.length;
+var numEntities = entities.length;
 
 var width = 1100,
     height = 700,
-    pad = 20,
-    left_pad = 150;
+    rightPad = 20,
+    leftPad = 150,
+    bottomPad = 20,
+    topPad = 20;
 
 //Add the empty svg element to the DOM
 var svg = d3.select("#scatterplot")
@@ -29,59 +70,53 @@ var svg = d3.select("#scatterplot")
     .attr("width", width)
     .attr("height", height);
 
-var xMax = d3.max(entities, function(d) {
+var relevanceMax = d3.max(entities, function(d) {
     return parseFloat(d.relevance);
 });
 
-var xMin = d3.min(entities, function(d) {
+var relevanceMin = d3.min(entities, function(d) {
     return parseFloat(d.relevance);
 });
 
-var yMax = d3.max(entities, function(d) {
-    return parseFloat(d.count);
+var countMax = d3.max(entities, function(d) {
+    return parseInt(d.count);
 });
 
-var yMin = d3.min(entities, function(d) {
-    return parseFloat(d.count);
+var countMin = d3.min(entities, function(d) {
+    return parseInt(d.count);
 });
 
-var rMin = 10;
+//X values map from [0, relevanceMax + 0.2] to [leftPad, width - rightPad]
+var xScale = d3.scaleLinear().domain([0, relevanceMax + 0.2]).range([leftPad, width - rightPad])
 
-var rMax = Math.sqrt(Math.sqrt(width * width + height * height)) - rMin;
+//Y values map from [0, countMax + 2] to
+var yScale = d3.scaleLinear().domain([0, countMax + 2]).range([height - bottomPad, topPad]);
+
+console.log("Max X (Relevance):" + relevanceMax);
+console.log("Min X (Relevance):" + relevanceMin);
+
+console.log("Max Y (Count):" + countMax);
+console.log("Min Y (Count):" + countMin);
 
 
-var xScale = d3.scaleLinear().domain([0, xMax + 0.2]).range([left_pad, width - pad])
+//A color is assigned to each entity type
+var colorValue = function(d) {
+    return d.type;
+};
+var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-var yScale = d3.scaleLinear().domain([0, yMax + 2]).range([height - pad * 2, pad]);
 
-console.log("Max X:" + xMax);
-console.log("Min X:" + xMin);
-
-console.log("Max Y:" + yMax);
-console.log("Min y:" + yMin);
-
-//var xAxis = d3.svg.axis();
-var xAxis = d3.axisBottom();
-xAxis.scale(xScale);
-//xAxis.orient("bottom");
-
-//var yAxis = d3.svg.axis();
-var yAxis = d3.axisLeft();
-yAxis.scale(yScale);
-//yAxis.orient("left");
-
-// setup fill color
-var cValue = function(d) {
-        return d.type;
-    };
-
-  var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-// add the tooltip area to the webpage
+//Add the tooltip area to the webpage
 var tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
+
+//Minimum radius of a datapoint
+var rMin = 10;
+
+//Maximum radius of a datapoint
+var rMax = Math.sqrt(Math.sqrt(width * width + height * height)) - rMin;
 
 //Add the points to the scatter plot and size them accordingly
 svg.selectAll("circle")
@@ -96,141 +131,176 @@ svg.selectAll("circle")
         return yScale(d.count);
     })
     .attr("r", function(d) {
-        var relevanceRatio = (d.relevance - xMin) / (xMax - xMin);
-        var countRatio = (d.count - yMin) / (yMax - yMin);
 
-        var scale = Math.sqrt(relevanceRatio * relevanceRatio + countRatio * countRatio)
+        //Normalize the relevance and count to a [0,1] range
+        var relevanceRatio = (d.relevance - relevanceMin) / (relevanceMax - relevanceMin);
+        var countRatio = (d.count - countMin) / (countMax - countMin);
+
+        //Calculate scale from root of sum of squares of relevanceRatio and countRatio
+        var scale = Math.sqrt(relevanceRatio * relevanceRatio + countRatio * countRatio);
 
         return rMax * scale + rMin;
     })
     .style("fill", function(d) {
-        return color(cValue(d));
+        //Color the datapoints according to their type
+        return color(colorValue(d));
     })
     .on("mouseover", function(d) {
 
-      var relevanceRatio = (d.relevance - xMin) / (xMax - xMin);
-      var countRatio = (d.count - yMin) / (yMax - yMin);
+        //Normalize the relevance and count to a [0,1] range
+        var relevanceRatio = (d.relevance - relevanceMin) / (relevanceMax - relevanceMin);
+        var countRatio = (d.count - countMin) / (countMax - countMin);
 
-      var scale = Math.sqrt(relevanceRatio * relevanceRatio + countRatio * countRatio)
+        //Calculate scale from root of sum of squares of relevanceRatio and countRatio
+        var scale = Math.sqrt(relevanceRatio * relevanceRatio + countRatio * countRatio);
 
-      var radius =  rMax * scale + rMin;
+        var radius = rMax * scale + rMin;
 
+        //Display tooltip with relevant information
         tooltip.transition()
             .duration(200)
             .style("opacity", .9);
         tooltip.html("<b>Type: " + d.type + "<br/>" +
-            "Text: " + d.text + "<br/>" +
-          "Count: " + d.count + "<br/>" +
-        "Relevance: " + d.relevance + "</b>")
+                "Text: " + d.text + "<br/>" +
+                "Count: " + d.count + "<br/>" +
+                "Relevance: " + d.relevance + "</b>")
             .style("left", (xScale(d.relevance) - 30) + "px")
             .style("top", (yScale(d.count) - radius - 20) + "px");
     })
     .on("mouseout", function(d) {
+
+        //Hide tooltip upon mouse-out
         tooltip.transition()
             .duration(500)
             .style("opacity", 0);
     });
 
 
-    function contrastingColor(color)
-    {
-        return (luma(color) >= 165) ? '000' : 'fff';
-    }
-    function luma(color) // color can be a hx string or an array of RGB values 0-255
-    {
-        var rgb = (typeof color === 'string') ? hexToRgb(color) : color;
-        return (0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2]); // SMPTE C, Rec. 709 weightings
-    }
+var labelFont = "16px sans-serif"
 
-    function hexToRgb(hex) {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
-    }
-
-
-
-//Add labels to the points to be able to see what the values are
+//Add labels to the points to be able to see what the associated text is
 svg.selectAll("text")
     .data(entities)
     .enter()
     .append("text")
     .text(function(d) {
+        console.log("width of " + d.text + " is: " + d.text.width('11px sans-serif'));
+        console.log("height of " + d.text + " is: " + d.text.height('11px sans-serif'));
 
         return d.text;
     })
+    .style("font", labelFont)
     .attr("x", function(d) {
 
-      var relevanceRatio = (d.relevance - xMin) / (xMax - xMin);
-      var countRatio = (d.count - yMin) / (yMax - yMin);
+        //Center the text on the datapoint's center
+        return xScale(d.relevance) - d.text.width(labelFont) / 2;
 
-      var scale = Math.sqrt(relevanceRatio * relevanceRatio + countRatio * countRatio)
-
-      var radius =  rMax * scale + rMin;
-
-        return xScale(d.relevance) - radius/2;
     })
     .attr("y", function(d) {
-        return yScale(d.count);
+
+        //Normalize the relevance and count to a [0,1] range
+        var relevanceRatio = (d.relevance - relevanceMin) / (relevanceMax - relevanceMin);
+        var countRatio = (d.count - countMin) / (countMax - countMin);
+
+        //Calculate scale from root of sum of squares of relevanceRatio and countRatio
+        var scale = Math.sqrt(relevanceRatio * relevanceRatio + countRatio * countRatio);
+
+        var radius = rMax * scale + rMin;
+
+        //Place the text just beneath the data-point
+        return yScale(d.count) + radius + d.text.height(labelFont) + 1;
     })
     .attr("fill", function(d) {
-    //  return "#000";
-        return contrastingColor(color(cValue(d)));
-    })
-    .attr("font-size", "11px");
+        // Fill with a color that contrasts well with the datapoint's fill color
+        return contrastingColor(color(colorValue(d)));
+    });
 
 
-// draw legend
+// Draw legend
 var legend = svg.selectAll(".legend")
     .data(color.domain())
-  .enter().append("g")
+    .enter().append("g")
     .attr("class", "legend")
-    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+    .attr("transform", function(d, i) {
+        return "translate(0," + i * 35 + ")";
+    });
 
-    // draw legend colored rectangles
+// draw legend colored rectangles
 legend.append("rect")
-.attr("x", width - 18)
-.attr("y", pad)
-.attr("width", 18)
-.attr("height", 18)
-.style("fill", color);
+    .attr("x", width - 20)
+    .attr("y", rightPad)
+    .attr("width", 20)
+    .attr("height", 20)
+    .style("fill", color);
 
 // draw legend text
 legend.append("text")
-.attr("x", width - 24)
-.attr("y", pad + 9)
-.attr("dy", ".35em")
-.style("text-anchor", "end")
-.text(function(d) { return d;})
+    .attr("x", width - 26)
+    .attr("y", rightPad + 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(function(d) {
+        return d;
+    })
     .attr("font-size", "14px");
 
-// x-axis
+
+var xAxis = d3.axisBottom();
+xAxis.scale(xScale);
+var xLabel = "Relevance";
+
+var yAxis = d3.axisLeft();
+yAxis.scale(yScale);
+var yLabel = "Count";
+
+// Add X-axis
 svg.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(0, " + (height - pad) + ")")
+    .attr("transform", "translate(0, " + (height - bottomPad) + ")")
     .call(xAxis)
     .append("text")
     .attr("class", "label")
-    .attr("x", width - pad)
-    .attr("y", -10)
-    .attr("fill", "#555")
+    .attr("x", width - rightPad)
+    .attr("y", -6)
+    .attr("fill", "#222")
     .style("text-anchor", "end")
-    .text("Relevance");
+    .style("font-size", "19px")
+    .text(xLabel);
 
-//y-axis
+//Add Y-axis
 svg.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(" + (left_pad - pad) + ", 0)")
+    .attr("transform", "translate(" + leftPad + ", 0)")
     .call(yAxis)
     .append("text")
     .attr("class", "label")
     .attr("transform", "rotate(-90)")
     .attr("y", 6)
-    .attr("x", -pad)
+    .attr("x", -topPad)
     .attr("dy", ".71em")
-    .attr("fill", "#555")
+    .attr("fill", "#333")
     .style("text-anchor", "end")
-    .text("Count");
+    .style("font-size", "19px")
+    .text(yLabel);
+
+
+//Return a color (either black or white) that contrasts well with the argument color
+function contrastingColor(color) {
+    return (luma(color) >= 165) ? '000' : 'fff';
+}
+
+//Return the relative luminance of the argument color
+function luma(color) {
+    var rgb = (typeof color === 'string') ? hexToRgb(color) : color;
+    return (0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2]); // SMPTE C, Rec. 709 weightings
+}
+
+//Conver a hex-formatted color to an RGB-formatted color
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
