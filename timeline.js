01a4da433f80eb69526ce81adb48c800fec5e03f
Omar Mukadam <Omar.Mukadam@Vonage.com>
2016-08-01 16:42:47 -0400

    Added x-axis correctly scaled to minTime and maxTime

--- timeline.js
+++ timeline.js
@@ -84,41 +84,33 @@ function displayGraph(inputString) {
         bottomPad = 40,
         topPad = 20;
 
-//var parseTime = d3.timeParse("%H:%M");
+    //var parseTime = d3.timeParse("%H:%M");
 
-var minTime = parseTime(entities[0].timestamp[0]);
-var maxTime = parseTime(entities[0].timestamp[0]);
+    var minTime = parseTime(entities[0].timestamp[0]);
+    var maxTime = parseTime(entities[0].timestamp[0]);
 
-for (var i = 0; i < entities.length; i++)
-{
-  for (var j = 0; j < entities[i].timestamp.length; j++)
-  {
-    //Converting time strings to valid Date objects
-    entities[i].timestamp[j] = parseTime(entities[i].timestamp[j]);
+    for (var i = 0; i < entities.length; i++) {
+        for (var j = 0; j < entities[i].timestamp.length; j++) {
+            //Converting time strings to valid Date objects
+            entities[i].timestamp[j] = parseTime(entities[i].timestamp[j]);
 
-    //Finding maxTime and minTime
-    if (maxTime < entities[i].timestamp[j]) maxTime = entities[i].timestamp[j];
-    else if (minTime > entities[i].timestamp[j]) minTime = entities[i].timestamp[j];
+            //Finding maxTime and minTime
+            if (maxTime < entities[i].timestamp[j]) maxTime = entities[i].timestamp[j];
+            else if (minTime > entities[i].timestamp[j]) minTime = entities[i].timestamp[j];
 
-  }
-}
+        }
+    }
 
-console.log("Parsed time for entities:\n\n" + JSON.stringify(entities, null, "\t"));
+    console.log("Parsed time for entities:\n\n" + JSON.stringify(entities, null, "\t"));
 
-console.log("Max time: " + maxTime);
-console.log("Min time: " + minTime);
-
-/*
-        var timeMax = d3.max(entities, function(d) {
-            return parseTime(d.timestamp);
-        });
-
-        var timeMin = d3.min(entities, function(d) {
-            return parseTime(d.timestamp);
-        });
+    console.log("Max time: " + maxTime);
+    console.log("Min time: " + minTime);
 
 
 
+    var xScale = d3.scaleTime()
+        .domain([minTime, maxTime])
+        .range([leftPad, width - rightPad]);
 
     var labelFont = "14px sans-serif bold";
 
@@ -130,201 +122,205 @@ console.log("Min time: " + minTime);
         .attr("height", height);
 
 
-    var relevanceMax = d3.max(entities, function(d) {
-        return parseFloat(d.relevance);
-    });
+    var xAxis = d3.axisBottom();
+    xAxis.scale(xScale);
+    var xLabel = "Time";
 
-    var relevanceMin = d3.min(entities, function(d) {
-        return parseFloat(d.relevance);
-    });
+xAxis.tickFormat(d3.timeFormat("%I %p"));
 
-    var countMax = d3.max(entities, function(d) {
-        return parseInt(d.count);
-    });
-
-    var countMin = d3.min(entities, function(d) {
-        return parseInt(d.count);
-    });
-
-
-
-    //From http://www.color-hex.com/color-palette/21490
-    var colorPalette = ["#15a071", "#b20000", "#00939f", "#ffae19", "#993299"];
-
-    //A color is assigned to each entity type
-    var colorValue = function(d) {
-        return d.type;
-    };
-    var color = d3.scaleOrdinal(colorPalette);
-
-
-    //Y values map from [0, countMax + 2] to
-    var yScale = d3.scaleLinear().domain([0, countMax + 1.5]).range([height - bottomPad, topPad]);
-
-
-    var yAxis = d3.axisLeft();
-    yAxis.scale(yScale);
-    var yLabel = "Count";
-
-    //Add the tooltip area to the webpage
-    var tooltip = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-
-
-    //Add bars
-    var bar = svg.selectAll(".bar")
-        .data(entities)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("width", barWidth + "px")
-        .attr("x", function(d, i) {
-            return i * (barWidth + spacing) + leftPad + offset;
-        })
-        .attr("y", function(d) {
-            return yScale(d.count);
-        })
-        .attr("height", function(d) {
-            return height - yScale(d.count) - bottomPad;
-        })
-        .attr("rx", 5)
-        .attr("ry", 5)
-        .style("fill", function(d) {
-            //Color the datapoints according to their type
-            return color(colorValue(d));
-        })
-        .style("stroke", "black")
-        .style("stroke-width", 1)
-        .on("mouseover", function(d, i) {
-
-            //Make the bar brighter with heavier borders
-            d3.select(this)
-                .style("opacity", "0.5")
-                .style("stroke-width", 5);
-
-            //Display tooltip with relevant information
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-            tooltip.html("<b>Type: " + d.type + "<br/>" +
-                    "Text: " + d.text + "<br/>" +
-                    "Count: " + d.count + "<br/>" +
-                    "Relevance: " + d.relevance + "</b>")
-                .style("left", i * (barWidth + spacing) + leftPad + offset + "px")
-                .style("top", (yScale(d.count) - 30) + "px");
-
-        })
-        .on("mouseout", function(d) {
-
-            //Reset the bar's opacity and border
-            d3.select(this)
-                .style("opacity", 1)
-                .style("stroke-width", 1);
-
-            //Hide tooltip upon mouse-out
-            tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });;
-
-
-    //Add labels to the points to be able to see what the associated text is
-    svg.selectAll("text")
-        .data(entities)
-        .enter()
-        .append("text")
-        .text(function(d) {
-
-            if (d.text.width(labelFont) < barWidth + spacing) return d.text;
-            else {
-                var temp = d.text;
-                temp.split(" ").join("\n");
-                return temp;
-            }
-        })
-        .style("font", labelFont)
-        .attr("x", function(d, i) {
-
-            var text = d.text;
-
-            if (d.text.width(labelFont) >= barWidth + spacing) {
-                text.replace(/\s/g, "\n");
-            }
-
-            //Center the text on the datapoint's center
-            return i * (barWidth + spacing) + leftPad + offset +
-                (barWidth - text.width(labelFont)) / 2;
-
-        })
-        .attr("y", function(d) {
-
-            return height - bottomPad + d.text.height(labelFont);
-        })
-        .style("font-weight", "bold");
-
-
-    function maxLabelWidth() {
-        var max = 0;
-        for (var i = 0; i < numEntities; i++) {
-            if (entities[i].text.width(labelFont) > max) {
-                max = entities[i].text.width(labelFont);
-            }
-        }
-        return max;
-    }
-
-    function updateWidth() {
-        return numEntities * (barWidth + spacing) + offset + maxLabelWidth() +
-            leftPad + rightPad;
-    }
-
-    width = updateWidth();
-
-    // Draw legend
-    var legend = svg.selectAll(".legend")
-        .data(color.domain())
-        .enter().append("g")
-        .attr("class", "legend")
-        .attr("transform", function(d, i) {
-            return "translate(0," + i * 35 + ")";
-        });
-
-    // draw legend colored rectangles
-    legend.append("rect")
-        .attr("x", width - 20)
-        .attr("y", rightPad)
-        .attr("width", 20)
-        .attr("height", 20)
-        .style("fill", color);
-
-    // draw legend text
-    legend.append("text")
-        .attr("x", width - 26)
-        .attr("y", rightPad + 9)
-        .attr("dy", ".35em")
-        .style("text-anchor", "end")
-        .text(function(d) {
-            return d;
-        })
-        .attr("font-size", "14px");
-
-
-    //Add Y-axis
+    // Add X-axis
     svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(" + leftPad + ", 0)")
-        .call(yAxis)
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0, " + (height - bottomPad) + ")")
+        .call(xAxis)
         .append("text")
         .attr("class", "label")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("x", -topPad)
-        .attr("dy", ".71em")
-        .attr("fill", "#333")
+        .attr("x", width - rightPad)
+        .attr("y", -6)
+        .attr("fill", "#222")
         .style("text-anchor", "end")
         .style("font-size", "19px")
-        .text(yLabel);
-*/
+        .text(xLabel);
+
+    /*
+        //From http://www.color-hex.com/color-palette/21490
+        var colorPalette = ["#15a071", "#b20000", "#00939f", "#ffae19", "#993299"];
+
+        //A color is assigned to each entity type
+        var colorValue = function(d) {
+            return d.type;
+        };
+        var color = d3.scaleOrdinal(colorPalette);
+
+
+        //Y values map from [0, countMax + 2] to
+        var yScale = d3.scaleLinear().domain([0, countMax + 1.5]).range([height - bottomPad, topPad]);
+
+
+        var yAxis = d3.axisLeft();
+        yAxis.scale(yScale);
+        var yLabel = "Count";
+
+        //Add the tooltip area to the webpage
+        var tooltip = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+    */
+    /*
+        //Add bars
+        var bar = svg.selectAll(".bar")
+            .data(entities)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("width", barWidth + "px")
+            .attr("x", function(d, i) {
+                return i * (barWidth + spacing) + leftPad + offset;
+            })
+            .attr("y", function(d) {
+                return yScale(d.count);
+            })
+            .attr("height", function(d) {
+                return height - yScale(d.count) - bottomPad;
+            })
+            .attr("rx", 5)
+            .attr("ry", 5)
+            .style("fill", function(d) {
+                //Color the datapoints according to their type
+                return color(colorValue(d));
+            })
+            .style("stroke", "black")
+            .style("stroke-width", 1)
+            .on("mouseover", function(d, i) {
+
+                //Make the bar brighter with heavier borders
+                d3.select(this)
+                    .style("opacity", "0.5")
+                    .style("stroke-width", 5);
+
+                //Display tooltip with relevant information
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                tooltip.html("<b>Type: " + d.type + "<br/>" +
+                        "Text: " + d.text + "<br/>" +
+                        "Count: " + d.count + "<br/>" +
+                        "Relevance: " + d.relevance + "</b>")
+                    .style("left", i * (barWidth + spacing) + leftPad + offset + "px")
+                    .style("top", (yScale(d.count) - 30) + "px");
+
+            })
+            .on("mouseout", function(d) {
+
+                //Reset the bar's opacity and border
+                d3.select(this)
+                    .style("opacity", 1)
+                    .style("stroke-width", 1);
+
+                //Hide tooltip upon mouse-out
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });;
+
+
+        //Add labels to the points to be able to see what the associated text is
+        svg.selectAll("text")
+            .data(entities)
+            .enter()
+            .append("text")
+            .text(function(d) {
+
+                if (d.text.width(labelFont) < barWidth + spacing) return d.text;
+                else {
+                    var temp = d.text;
+                    temp.split(" ").join("\n");
+                    return temp;
+                }
+            })
+            .style("font", labelFont)
+            .attr("x", function(d, i) {
+
+                var text = d.text;
+
+                if (d.text.width(labelFont) >= barWidth + spacing) {
+                    text.replace(/\s/g, "\n");
+                }
+
+                //Center the text on the datapoint's center
+                return i * (barWidth + spacing) + leftPad + offset +
+                    (barWidth - text.width(labelFont)) / 2;
+
+            })
+            .attr("y", function(d) {
+
+                return height - bottomPad + d.text.height(labelFont);
+            })
+            .style("font-weight", "bold");
+
+
+        function maxLabelWidth() {
+            var max = 0;
+            for (var i = 0; i < numEntities; i++) {
+                if (entities[i].text.width(labelFont) > max) {
+                    max = entities[i].text.width(labelFont);
+                }
+            }
+            return max;
+        }
+
+        function updateWidth() {
+            return numEntities * (barWidth + spacing) + offset + maxLabelWidth() +
+                leftPad + rightPad;
+        }
+
+        width = updateWidth();
+
+        // Draw legend
+        var legend = svg.selectAll(".legend")
+            .data(color.domain())
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) {
+                return "translate(0," + i * 35 + ")";
+            });
+
+        // draw legend colored rectangles
+        legend.append("rect")
+            .attr("x", width - 20)
+            .attr("y", rightPad)
+            .attr("width", 20)
+            .attr("height", 20)
+            .style("fill", color);
+
+        // draw legend text
+        legend.append("text")
+            .attr("x", width - 26)
+            .attr("y", rightPad + 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(function(d) {
+                return d;
+            })
+            .attr("font-size", "14px");
+
+
+        //Add Y-axis
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + leftPad + ", 0)")
+            .call(yAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("x", -topPad)
+            .attr("dy", ".71em")
+            .attr("fill", "#333")
+            .style("text-anchor", "end")
+            .style("font-size", "19px")
+            .text(yLabel);
+    */
 }
 
 function reset() {
@@ -375,8 +371,7 @@ function parseTime(timeStr, dt) {
     var hours = parseInt(time[1], 10);
     if (hours == 12 && !time[3]) {
         hours = 0;
-    }
-    else {
+    } else {
         hours += (hours < 12 && time[3]) ? 12 : 0;
     }
 
